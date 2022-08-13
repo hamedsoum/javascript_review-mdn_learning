c37b4574397a2aa5579882599bd28dad80e7269a
hamedsoum <hamedsoum4023@gmail.com>
2022-08-13 13:21:48 +0000

    -first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+let para = document.querySelector('p');
+para.addEventListener('click', updateName);
+
+function updateName() {
+  let name = prompt('Enter a new name');
+  para.textContent = 'Player 1: ' + name;
+}
