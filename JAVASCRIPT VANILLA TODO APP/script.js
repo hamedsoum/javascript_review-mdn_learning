5e6aaa3c4c6cc700ec9e21b35f6a5b6403beeecd
hamedsoum <hamedsoum4023@gmail.com>
2022-08-22 14:30:40 +0000

    update deature in progress

--- JAVASCRIPT VANILLA TODO APP/script.js
+++ JAVASCRIPT VANILLA TODO APP/script.js
@@ -2,27 +2,13 @@ itemsTab = ['task1', 'task2', 'task3', 'task4'];
 
 const itemsContent = document.querySelector('.items'); 
 
-
 let taskValue = '';
-
-
-// itemsTab.forEach((el, i) => {
-//     const itemDiv = document.createElement('div');
-//     itemDiv.setAttribute('class', 'item')
-//     itemDiv.innerHTML = `<span>${el}</span> <i class="fa fa-trash" onClick='deleteTask(${i})' aria-hidden="true"></i>  <i class="fa fa-pen" onClick='updateTask(${i})' aria-hidden="true"></i>`;
-//     // const deleteButton = document.createElement('i');
-//     // deleteButton.setAttribute('class', 'fa fa-pen')
-//     itemsContent.appendChild(itemDiv);
-// });
 
 function addTask(event){
     const itemDiv = document.createElement('div');
     itemDiv.setAttribute('class', 'item')
         itemDiv.setAttribute('class', 'item')
-
-    itemDiv.innerHTML = `<span>${taskValue}</span> <i class="fa fa-trash" aria-hidden="true"></i>  <i class="fa fa-pen"  aria-hidden="true"></i>`;
-    // const deleteButton = document.createElement('i');
-    // deleteButton.setAttribute('class', 'fa fa-pen')
+    itemDiv.innerHTML = `<span>${taskValue}</span> <i class="fa fa-trash" aria-hidden="true"></i> <i class="fa fa-pen" aria-hidden="true"></i>`;
     itemsContent.appendChild(itemDiv);
     console.log(taskValue);
     itemsTab.push(taskValue);
@@ -33,21 +19,25 @@ function addTask(event){
     deleteBtn.addEventListener('click', function() {
         itemDiv.remove();
     })
-    updateBtn.addEventListener('click', function() {
-        console.log('update button');
+    updateBtn.addEventListener('click', function(event) {
+        spanValue = itemDiv.children[0].textContent;
+        console.log(event.target);
+        itemDiv.innerHTML = ` <input  type="text" value ='${spanValue}'><button type="submit"><i class="fa fa-pen" aria-hidden="true"></i></button>`;
+        updateInput = itemDiv.children[0];
+        saveUpdateBtn = itemDiv.children[1];
+        console.log(updateInput);
+        updateInput.addEventListener('change', function(event) {
+            updateValue = event.target.value;
+        })
+        saveUpdateBtn.addEventListener('click', function(){
+            itemDiv.innerHTML = `<span>${taskValue}</span> <i class="fa fa-trash" aria-hidden="true"></i>  <i class="fa fa-pen" aria-hidden="true"></i>`;
+        });
+        console.log(spanValue);
     })
 }
-
 function getTaskName(event){
     taskValue = event.target.value;
 }
-
 function updateTask(el){
     taskValue = el;
-
 }
-
-// function deleteTask(event){
-//   var x = document.getElementsByTagName("i")[0].parentElement;
-//   x.remove();
-// }
